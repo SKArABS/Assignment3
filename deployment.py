cb28768e229f5dbb8f6ecddf3012a2c6a57f7cd7
SKArABS <123462279+SKArABS@users.noreply.github.com>
2023-11-17 22:52:06 +0000

    Add files via upload

--- deployment.py
+++ deployment.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import numpy as np
-import sklearn
-import keras
-from keras.models import *
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from keras.models import load_model
 import pickle
 
 # Load the Keras model
@@ -31,14 +30,14 @@ user_input_payment_method = st.selectbox("Select a payment method:", ["Electroni
 user_input_monthly_charges = st.number_input("Enter a numerical value:", min_value=0.0)
 
 # Preprocess user input
-encoded_user_input_senior_citizen = label_encoder.transform([user_input_senior_citizen])
-encoded_user_input_phone_service = label_encoder.transform([user_input_phone_service])
-encoded_user_input_multiple_lines = label_encoder.transform([user_input_multiple_lines])
-encoded_user_input_streaming_tv = label_encoder.transform([user_input_streaming_tv])
-encoded_user_input_streaming_movies = label_encoder.transform([user_input_streaming_movies])
-encoded_user_input_paperless_billing = label_encoder.transform([user_input_paperless_billing])
-encoded_user_input_payment_method = label_encoder.transform([user_input_payment_method])
-scaled_user_monthly_charges = scaler.transform([[user_input_monthly_charges]])
+encoded_user_input_senior_citizen = label_encoder.fit_transfor([user_input_senior_citizen])
+encoded_user_input_phone_service = label_encoder.fit_transform([user_input_phone_service])
+encoded_user_input_multiple_lines = label_encoder.fit_transform([user_input_multiple_lines])
+encoded_user_input_streaming_tv = label_encoder.fit_transform([user_input_streaming_tv])
+encoded_user_input_streaming_movies = label_encoder.fit_transform([user_input_streaming_movies])
+encoded_user_input_paperless_billing = label_encoder.fit_transform([user_input_paperless_billing])
+encoded_user_input_payment_method = label_encoder.fit_transform([user_input_payment_method])
+scaled_user_monthly_charges = scaler.fit_transform([[user_input_monthly_charges]])
 
 # Make predictions
 predictions = model.predict([encoded_user_input_senior_citizen, encoded_user_input_phone_service, 
